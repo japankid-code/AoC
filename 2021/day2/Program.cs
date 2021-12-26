5aede54b2aa453870d681881871eda7c55839f70
jake rankin <japankid.jake@gmail.com>
2021-12-26 15:25:22 -0600

    Day 2 and day 3 parts 1 and 2

---
+++ 2021/day2/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+
+Console.WriteLine("Hello, World!");
+var lines = File.ReadAllLines(@"C:\Users\jrank\Documents\GitHub\japankid-code\AoC\2021\day2\input.txt").ToList();
+
+#region Part 1
+    Dictionary<string, int> pairs = new ();
+    var lkv = lines.Select(l => new { k = l.Split(' ')[0], v = int.Parse(l.Split(' ')[1]) });
+    foreach (var kv in lkv) {
+        if (pairs.ContainsKey(kv.k)) pairs[kv.k] = pairs[kv.k] + kv.v;
+        else pairs.Add(kv.k, kv.v);
+    }
+
+    var h = pairs["forward"];
+    var v = pairs["down"] - pairs["up"];
+
+    foreach (var pair in pairs) Console.WriteLine($"{pair.Key} : {pair.Value}");
+
+    var hxv = h * v;
+    Console.WriteLine($"part 1 = {hxv}\n\n\n");
+#endregion
+
+#region Part 2
+    var aim = 0;
+    var depth = 0;
+    var horizontal = 0;
+    foreach (var kv in lkv) {
+        if (kv.k == "down") aim += kv.v;
+        else if (kv.k == "up") aim -= kv.v;
+        else if (kv.k == "forward") {
+            depth += kv.v * aim;
+            horizontal += kv.v;
+        }
+    }
+    Console.WriteLine($"horizontal x depth = {horizontal * depth}");
+#endregion
---
+++ 2021/day3/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] lines = File.ReadAllLines(@"C:\Users\jrank\Documents\GitHub\japankid-code\AoC\2021\day3\input.txt");
+
+char BitOne = char.Parse("1");
+char BitZero = char.Parse("0");
+
+#region Part 1
+var columns = new Dictionary<int, KeyValuePair<int, int>>() { };
+foreach ( var line in lines ) {
+    for ( int col = 0; col < line.Length; col++ ) {
+        int addOne = Convert.ToInt32(line[col] == '1');
+        int addZero = Convert.ToInt32(line[col] == '0');
+        if ( !columns.ContainsKey(col) ) columns.Add(col, new KeyValuePair<int, int>(addOne, addZero));
+        var totalOnes = columns[col].Key + addOne;
+        var totalZeros = columns[col].Value + addZero;
+        columns[col] = new KeyValuePair<int, int>(totalOnes, totalZeros);
+    }
+}
+string gr = "";
+string er = "";
+foreach ( var column in columns ) {
+    int nOfOnes = column.Value.Key;
+    int nOfZeros = column.Value.Value;
+    Console.WriteLine($"{column.Key}, {nOfOnes}, {nOfZeros}");
+    gr = nOfOnes > nOfZeros ? gr += "1" : gr += "0";
+    er = nOfOnes < nOfZeros ? er += "1" : er += "0";
+}
+int g = Convert.ToInt32(gr, 2);
+int e = Convert.ToInt32(er, 2);
+Console.WriteLine($"gamma rate: {g}");
+Console.WriteLine($"epsilon rate: {e}");
+Console.WriteLine($"power consumption: {g * e}\n\n\n");
+#endregion
+
+#region Part 2
+int ogr = 0;
+int csr = 0;
+string[] linesForOxygen = lines;
+string[] linesForCo2 = lines;
+for ( int col = 0; col < 12; col++ ) {
+    linesForOxygen = Selector(linesForOxygen, col, true);
+    if (linesForOxygen.Length == 1) {
+        var finalLeft = linesForOxygen.FirstOrDefault();
+        ogr = Convert.ToInt32(finalLeft, 2);
+    }
+
+    linesForCo2 = Selector(linesForCo2, col, false);
+    if (linesForCo2.Length == 1) {
+        var finalRight = linesForCo2.FirstOrDefault();
+        csr = Convert.ToInt32(finalRight, 2);
+    }
+}
+
+string[] Selector(string[] remaining, int col, bool defaultVal) {
+    var resultRight = remaining.Where(line => line[col].Equals(BitOne)).ToArray();
+    var resultLeft = remaining.Where(line => line[col].Equals(BitZero)).ToArray();
+
+    if (resultRight.Length != resultLeft.Length) {
+        // return the more common for osr
+        if (defaultVal) return resultRight.Length > resultLeft.Length ? resultRight : resultLeft;
+        // return the less common for csr
+        return resultRight.Length > resultLeft.Length ? resultLeft : resultRight;
+    } else return defaultVal ? resultRight : resultLeft;
+}
+
+Console.WriteLine($"oxygen generator rating: {ogr}");
+Console.WriteLine($"Co2 scrubber rating: {csr}");
+Console.WriteLine($"life support rating: {ogr * csr}\n\n\n");
+#endregion
